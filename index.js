74bb3add664157bddb069e38afa1ced9edfc4623
Glad Chinda <gladxeqs@gmail.com>
2018-04-13 15:07:20 +0100

    Added alias methods

--- index.js
+++ index.js
@@ -11,18 +11,24 @@ var withMatrix = function(method) {
 };
 
 var _matrix = {
-	ok: withMatrix('ok'),
-	dimension: withMatrix('dimension'),
-	unit: withoutMatrix('unit'),
-	equal: withoutMatrix('equal'),
-	sum: withoutMatrix('sum'),
-	product: withoutMatrix('product'),
-	transpose: withMatrix('transpose'),
-	determinant: withMatrix('determinant'),
-	minors: withMatrix('minors'),
-	cofactors: withMatrix('cofactors'),
-	adjoint: withMatrix('adjoint'),
-	inverse: withMatrix('inverse')
+	ok: withMatrix("ok"),
+	valid: withMatrix("ok"),
+	order: withMatrix("dimension"),
+	dimension: withMatrix("dimension"),
+	unit: withoutMatrix("unit"),
+	identity: withoutMatrix("unit"),
+	equal: withoutMatrix("equal"),
+	same: withoutMatrix("equal"),
+	sum: withoutMatrix("sum"),
+	add: withoutMatrix("sum"),
+	product: withoutMatrix("product"),
+	multiply: withoutMatrix("product"),
+	transpose: withMatrix("transpose"),
+	determinant: withMatrix("determinant"),
+	minors: withMatrix("minors"),
+	cofactors: withMatrix("cofactors"),
+	adjoint: withMatrix("adjoint"),
+	inverse: withMatrix("inverse")
 };
 
 if (typeof window !== "undefined") {
