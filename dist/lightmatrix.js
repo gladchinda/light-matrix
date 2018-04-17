ae35f58c780ec6acd68fe6fd313ed8c17b6485ea
Glad Chinda <gladxeqs@gmail.com>
2018-04-17 11:43:58 +0100

    Fixed a transpiling issue for dist

--- dist/lightmatrix.js
+++ dist/lightmatrix.js
@@ -4,34 +4,46 @@
   (global.lightmatrix = factory());
 }(this, (function () { 'use strict';
 
-  const isNumber = num => typeof num === 'number' && !isNaN(num);
+  function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
 
-  const isNumeric = num => {
-    const number = Number(String(num instanceof Object ? true : num));
+  var isNumber = function isNumber(num) {
+    return typeof num === 'number' && !isNaN(num);
+  };
+
+  var isNumeric = function isNumeric(num) {
+    var number = Number(String(num instanceof Object ? true : num));
     return isNumber(number);
   };
 
-  const reciprocal = num => 1 / num;
+  var reciprocal = function reciprocal(num) {
+    return 1 / num;
+  };
 
-  const normalizeNumber = num => num === 0 ? 0 : num;
+  var normalizeNumber = function normalizeNumber(num) {
+    return num === 0 ? 0 : num;
+  };
 
-  const isMatrix = matrix => {
+  var isMatrix = function isMatrix(matrix) {
 
     if (!(matrix instanceof Array) || matrix.length === 0) return false;
 
-    let cols;
-    const [...cloneMatrix] = matrix;
+    var cols = void 0;
+
+    var _matrix = _toArray(matrix),
+        cloneMatrix = _matrix.slice(0);
 
     while (cloneMatrix.length > 0) {
-      const row = cloneMatrix.shift();
-      const isArrayRow = row instanceof Array;
+      var row = cloneMatrix.shift();
+      var isArrayRow = row instanceof Array;
 
       if (!isArrayRow) return false;
 
       cols = cols || row.length;
 
-      const sameRowCols = row.length === cols;
-      const numericRowElements = row.filter(e => isNumeric(e)).length === cols;
+      var sameRowCols = row.length === cols;
+      var numericRowElements = row.filter(function (e) {
+        return isNumeric(e);
+      }).length === cols;
 
       if (!(sameRowCols && numericRowElements)) return false;
     }
@@ -39,13 +51,13 @@
     return true;
   };
 
-  const pivotMatrix = (matrix, row, col) => {
-    const newMatrix = [];
+  var pivotMatrix = function pivotMatrix(matrix, row, col) {
+    var newMatrix = [];
 
-    for (let i = 0; i < matrix.length; i += 1) {
+    for (var i = 0; i < matrix.length; i += 1) {
       if (i !== row) {
-        const newRow = [];
-        for (let j = 0; j < matrix[0].length; j += 1) {
+        var newRow = [];
+        for (var j = 0; j < matrix[0].length; j += 1) {
           if (j !== col) {
             newRow.push(normalizeNumber(matrix[i][j]));
           }
@@ -57,12 +69,14 @@
     return newMatrix;
   };
 
-  const scalarProduct = (n, matrix) => {
-    const [...newMatrix] = matrix;
-    const dim = [matrix.length, matrix[0].length];
+  var scalarProduct = function scalarProduct(n, matrix) {
+    var _matrix2 = _toArray(matrix),
+        newMatrix = _matrix2.slice(0);
 
-    for (let i = 0; i < dim[0]; i += 1) {
-      for (let j = 0; j < dim[1]; j += 1) {
+    var dim = [matrix.length, matrix[0].length];
+
+    for (var i = 0; i < dim[0]; i += 1) {
+      for (var j = 0; j < dim[1]; j += 1) {
         newMatrix[i][j] = normalizeNumber(matrix[i][j] * n);
       }
     }
@@ -70,19 +84,19 @@
     return newMatrix;
   };
 
-  const matrixProduct = (matA, matB) => {
+  var matrixProduct = function matrixProduct(matA, matB) {
 
-    const dimA = [matA.length, matA[0].length];
-    const dimB = [matB.length, matB[0].length];
-    const matrix = [];
+    var dimA = [matA.length, matA[0].length];
+    var dimB = [matB.length, matB[0].length];
+    var matrix = [];
 
     if (dimA[1] === dimB[0]) {
-      for (let i = 0; i < dimA[0]; i += 1) {
+      for (var i = 0; i < dimA[0]; i += 1) {
         matrix[i] = [];
-        for (let j = 0; j < dimB[1]; j += 1) {
-          let s = 0;
+        for (var j = 0; j < dimB[1]; j += 1) {
+          var s = 0;
 
-          for (let k = 0; k < dimA[1]; k += 1) {
+          for (var k = 0; k < dimA[1]; k += 1) {
             s += matA[i][k] * matB[k][j];
           }
 
@@ -95,23 +109,23 @@
     throw new Error('Matrices not compatible.');
   };
 
-  const sum = (a, b) => {
+  var sum = function sum(a, b) {
 
     if (!(isMatrix(a) && isMatrix(b))) {
       throw new Error('Invalid matrix specification.');
     }
 
-    const matrix = [];
-    const dimA = [a.length, a[0].length];
-    const dimB = [b.length, b[0].length];
+    var matrix = [];
+    var dimA = [a.length, a[0].length];
+    var dimB = [b.length, b[0].length];
 
     if (!(dimA[0] === dimB[0] && dimA[1] === dimB[1])) {
       throw new Error('Matrices not compatible.');
     }
 
-    for (let i = 0; i < dimA[0]; i += 1) {
+    for (var i = 0; i < dimA[0]; i += 1) {
       matrix[i] = [];
-      for (let j = 0; j < dimA[1]; j += 1) {
+      for (var j = 0; j < dimA[1]; j += 1) {
         matrix[i][j] = normalizeNumber(a[i][j] + b[i][j]);
       }
     }
@@ -119,7 +133,7 @@
     return matrix;
   };
 
-  const product = (a, b) => {
+  var product = function product(a, b) {
 
     if (isNumber(a)) {
       if (isNumber(b)) return a * b;else if (isMatrix(b)) return scalarProduct(a, b);
@@ -134,35 +148,33 @@
 
     ok: isMatrix,
 
-    dimension(matrix) {
+    dimension: function dimension(matrix) {
       if (this.ok(matrix)) {
         return [matrix.length, matrix[0].length];
       }
       throw new Error('Invalid matrix specification.');
     },
+    unit: function unit(dimension) {
+      var dim = dimension >= 1 && dimension || 2;
+      var unitmatrix = [];
 
-    unit(dimension) {
-      const dim = dimension >= 1 && dimension || 2;
-      const unitmatrix = [];
-
-      for (let i = 0; i < dim; i += 1) {
+      for (var i = 0; i < dim; i += 1) {
         unitmatrix[i] = [];
-        for (let j = 0; j < dim; j += 1) {
+        for (var j = 0; j < dim; j += 1) {
           unitmatrix[i][j] = i === j ? 1 : 0;
         }
       }
 
       return unitmatrix;
     },
-
-    equal(a, b) {
-      const dimA = this.dimension(a);
-      const dimB = this.dimension(b);
+    equal: function equal(a, b) {
+      var dimA = this.dimension(a);
+      var dimB = this.dimension(b);
 
       if (!(dimA[0] === dimB[0] && dimA[1] === dimB[1])) return false;
 
-      for (let i = 0; i < dimA[0]; i += 1) {
-        for (let j = 0; j < dimA[1]; j += 1) {
+      for (var i = 0; i < dimA[0]; i += 1) {
+        for (var j = 0; j < dimA[1]; j += 1) {
           if (a[i][j] !== b[i][j]) return false;
         }
       }
@@ -170,88 +182,85 @@
       return true;
     },
 
-    sum,
 
-    product,
+    sum: sum,
 
-    transpose(matrix) {
-      const dim = this.dimension(matrix).reverse();
-      const newmatrix = [];
+    product: product,
 
-      for (let i = 0; i < dim[0]; i += 1) {
+    transpose: function transpose(matrix) {
+      var dim = this.dimension(matrix).reverse();
+      var newmatrix = [];
+
+      for (var i = 0; i < dim[0]; i += 1) {
         newmatrix[i] = [];
-        for (let j = 0; j < dim[1]; j += 1) {
+        for (var j = 0; j < dim[1]; j += 1) {
           newmatrix[i][j] = normalizeNumber(matrix[j][i]);
         }
       }
 
       return newmatrix;
     },
+    determinant: function determinant(matrix) {
 
-    determinant(matrix) {
-
-      let det;
-      const dim = this.dimension(matrix);
+      var det = void 0;
+      var dim = this.dimension(matrix);
 
       if (dim[0] !== dim[1]) throw new Error('Square matrix required.');
 
       if (dim[0] === 1) return matrix[0][0];
 
-      for (let i = 0; i < matrix[0].length; i += 1) {
+      for (var i = 0; i < matrix[0].length; i += 1) {
         det = det || 0;
         det += ((i & 1) === 0 ? 1 : -1) * matrix[0][i] * this.determinant(pivotMatrix(matrix, 0, i));
       }
 
       return det;
     },
+    minors: function minors(matrix) {
 
-    minors(matrix) {
-
-      const dim = this.dimension(matrix);
-      const newmatrix = [];
+      var dim = this.dimension(matrix);
+      var newmatrix = [];
 
       if (dim[0] !== dim[1]) throw new Error('Square matrix required.');
 
-      for (let i = 0; i < dim[0]; i += 1) {
+      for (var i = 0; i < dim[0]; i += 1) {
         newmatrix[i] = [];
-        for (let j = 0; j < dim[0]; j += 1) {
+        for (var j = 0; j < dim[0]; j += 1) {
           newmatrix[i][j] = normalizeNumber(this.determinant(pivotMatrix(matrix, i, j)));
         }
       }
 
       return newmatrix;
     },
+    cofactors: function cofactors(matrix) {
+      var dim = this.dimension(matrix);
+      var newmatrix = this.minors(matrix);
 
-    cofactors(matrix) {
-      const dim = this.dimension(matrix);
-      const newmatrix = this.minors(matrix);
-
-      for (let i = 0; i < dim[0]; i += 1) {
-        for (let j = 0; j < dim[0]; j += 1) {
+      for (var i = 0; i < dim[0]; i += 1) {
+        for (var j = 0; j < dim[0]; j += 1) {
           newmatrix[i][j] = normalizeNumber(newmatrix[i][j] * ((i + j & 1) === 0 ? 1 : -1));
         }
       }
 
       return newmatrix;
     },
-
-    adjoint(matrix) {
+    adjoint: function adjoint(matrix) {
       return this.transpose(this.cofactors(matrix));
     },
-
-    inverse(matrix) {
-      const det = this.determinant(matrix);
+    inverse: function inverse(matrix) {
+      var det = this.determinant(matrix);
 
       if (det === 0) throw new Error('Cannot compute inverse of singular matrix.');
 
       return this.product(reciprocal(det), this.adjoint(matrix));
     }
-
   };
 
-  const bindMatrix = method => Matrix[method].bind(Matrix);
+  var bindMatrix = function bindMatrix(method) {
+    return Matrix[method].bind(Matrix);
+  };
 
-  const lightmatrix = {
+  var lightmatrix = {
     ok: bindMatrix('ok'),
     valid: bindMatrix('ok'),
     order: bindMatrix('dimension'),
